9333124b3d883c1ff813d965825e3c2794ca0808
ksanthippos <mikael.rauhala@eduespoo.fi>
2020-06-30 22:17:47 +0300

    2.18 valmis

---
+++ osa_2/puhelinluettelo 3/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./App";
+
+// OSA 2
+// PUHELINLUETTELO
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
+
