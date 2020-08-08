c51db704b20d9cff7411333d2a2784510b007cad
ksanthippos <mikael.rauhala@eduespoo.fi>
2020-08-08 09:03:53 +0300

    kopioitu projekti ja aloitettu 6.1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from "./App";
-import "./index.css"
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import reducer from './reducer'
 
-// OSA 3
-// PUHELINLUETTELO
+const store = createStore(reducer)
 
+const App = () => {
+  const good = () => {
+    store.dispatch({
+      type: 'GOOD'
+    })
+  }
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-);
+  return (
+    <div>
+      <button onClick={good}>good</button> 
+      <button>neutral</button> 
+      <button>bad</button>
+      <button>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>neutral</div>
+      <div>bad</div>
+    </div>
+  )
+}
 
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById('root'))
+}
+
+renderApp()
+store.subscribe(renderApp)
