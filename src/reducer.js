875ec004800a1f52407efe861432f95a094acc5d
mikaelrauhala <mikael.rauhala@eduespoo.fi>
2020-08-08 10:50:57 +0300

    reducer valmis

--- src/reducer.js
+++ src/reducer.js
@@ -6,15 +6,22 @@ const initialState = {
 
 const counterReducer = (state = initialState, action) => {
   console.log(action)
+  const currentState = {...state}
   switch (action.type) {
     case 'GOOD':
-      return state
+      currentState.good++
+      return currentState
     case 'OK':
-      return state
+      currentState.ok++
+      return currentState
     case 'BAD':
-      return state
+      currentState.bad++
+      return currentState
     case 'ZERO':
-      return state
+      currentState.good = 0
+      currentState.ok = 0
+      currentState.bad = 0
+      return currentState
     default: return state
   }
   
