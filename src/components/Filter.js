ec1df1cdca93f6b7c3c820349a41480360713f1f
ksanthippos <mikael.rauhala@eduespoo.fi>
2020-08-15 17:28:41 +0300

    aloitettu 6.12

---
+++ src/components/Filter.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Filter = () => {
+  const handleChange = (event) => {
+    // input-kentän arvo muuttujassa event.target.value
+  }
+  const style = {
+    marginBottom: 10
+  }
+
+  return (
+      <div style={style}>
+        filter <input onChange={handleChange} />
+      </div>
+  )
+}
+
+export default Filter
--- src/store.js
+++ src/store.js
@@ -3,10 +3,12 @@ import { composeWithDevTools } from "redux-devtools-extension/index";
 import thunk from "redux-thunk";
 import anecdoteReducer from "./reducers/anecdoteReducer";
 import notificationReducer from "./reducers/notificationReducer";
+import filterReducer from "./reducers/filterReducer";
 
 const reducer = combineReducers({
   anecdote: anecdoteReducer,
-  notification: notificationReducer
+  notification: notificationReducer,
+  filter: filterReducer
 })
 
 const store = createStore(
