067a21d55188d5088e995616c9375fa015b36c28
ksanthippos <mikael.rauhala@eduespoo.fi>
2020-08-24 18:57:27 +0300

    uusi branchi osalle 7

--- src/App.js
+++ src/App.js
@@ -1,75 +1,137 @@
-import React, {useEffect, useState} from 'react'
-import AddPerson from "./components/AddPerson";
-import FindName from "./components/FindName";
-import Display from "./components/Display";
-import Notification from "./components/Notification";
-import personService from "./services/persons"
+import React, { useState } from 'react'
 
-// OSA 2
-// PUHELINLUETTELO
-// tehtävät 2.19 - 2.20
-
-const App = () => {
-
-    const [ persons, setPersons] = useState([])
-    const [ newName, setNewName ] = useState('')
-    const [ newNumber, setNewNumber ] = useState('')
-    const [ searchTerm, setSearchTerm ] = useState('')
-    const [ searchResults, setSearchResults ] = useState([])
-    const [ searchNull, setSearchNull ] = useState(true)
-    const [ notificationClass, setNotificationClass ] = useState('success')
-    const [ notification, setNotification ] = useState(null, notificationClass)
-
-    useEffect(() => {
-        personService
-            .getAll()
-            .then(returnedPersons => {
-                setPersons(returnedPersons)
-            })
-    }, [])
-
-    const handleNameChange = (event) => { setNewName(event.target.value) }
-
-    const handleNumberChange = (event) => { setNewNumber(event.target.value) }
-
-    const handleFindName = (event) => { setSearchTerm(event.target.value) }
-
-    return (
-        <div>
-            <h1>Phonebook</h1>
-            <Notification message={notification} notificationClass={notificationClass}/>
-            <FindName setSearchNull={setSearchNull}
-                      persons={persons}
-                      setSearchResults={setSearchResults}
-                      searchTerm={searchTerm}
-                      handleFindName={handleFindName}
-            />
-            <h2>Add new</h2>
-            <AddPerson namesToShow={searchResults}
-                       persons={persons}
-                       setPersons={setPersons}
-                       setNewName={setNewName}
-                       newName={newName}
-                       handleNameChange={handleNameChange}
-                       newNumber={newNumber}
-                       setNewNumber={setNewNumber}
-                       handleNumberChange={handleNumberChange}
-                       setNotification={setNotification}
-                       notificationClass={notificationClass}
-                       setNotificationClass={setNotificationClass}
-            />
-            <h2>Numbers</h2>
-            <Display searchNull={searchNull}
-                     persons={persons}
-                     setPersons={setPersons}
-                     namesToShow={searchResults}
-                     setNotification={setNotification}
-                     notificationClass={notificationClass}
-                     setNotificationClass={setNotificationClass}
-            />
-            ...
-        </div>
-    )
+const Menu = () => {
+  const padding = {
+    paddingRight: 5
+  }
+  return (
+    <div>
+      <a href='#' style={padding}>anecdotes</a>
+      <a href='#' style={padding}>create new</a>
+      <a href='#' style={padding}>about</a>
+    </div>
+  )
 }
 
-export default App
+const AnecdoteList = ({ anecdotes }) => (
+  <div>
+    <h2>Anecdotes</h2>
+    <ul>
+      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+    </ul>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About anecdote app</h2>
+    <p>According to Wikipedia:</p>
+
+    <em>An anecdote is a brief, revealing account of an individual person or an incident.
+      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
+      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
+      An anecdote is "a story with a point."</em>
+
+    <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
+  </div>
+)
+
+const Footer = () => (
+  <div>
+    Anecdote app for <a href='https://courses.helsinki.fi/fi/tkt21009'>Full Stack -websovelluskehitys</a>.
+
+    See <a href='https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js'>https://github.com/fullstack-hy2019/routed-anecdotes/blob/master/src/App.js</a> for the source code.
+  </div>
+)
+
+const CreateNew = (props) => {
+  const [content, setContent] = useState('')
+  const [author, setAuthor] = useState('')
+  const [info, setInfo] = useState('')
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    props.addNew({
+      content,
+      author,
+      info,
+      votes: 0
+    })
+  }
+
+  return (
+    <div>
+      <h2>create a new anecdote</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          content
+          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+        </div>
+        <div>
+          author
+          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+        </div>
+        <div>
+          url for more info
+          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+        </div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+
+}
+
+const App = () => {
+  const [anecdotes, setAnecdotes] = useState([
+    {
+      content: 'If it hurts, do it more often',
+      author: 'Jez Humble',
+      info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
+      votes: 0,
+      id: '1'
+    },
+    {
+      content: 'Premature optimization is the root of all evil',
+      author: 'Donald Knuth',
+      info: 'http://wiki.c2.com/?PrematureOptimization',
+      votes: 0,
+      id: '2'
+    }
+  ])
+
+  const [notification, setNotification] = useState('')
+
+  const addNew = (anecdote) => {
+    anecdote.id = (Math.random() * 10000).toFixed(0)
+    setAnecdotes(anecdotes.concat(anecdote))
+  }
+
+  const anecdoteById = (id) =>
+    anecdotes.find(a => a.id === id)
+
+  const vote = (id) => {
+    const anecdote = anecdoteById(id)
+
+    const voted = {
+      ...anecdote,
+      votes: anecdote.votes + 1
+    }
+
+    setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
+  }
+
+  return (
+    <div>
+      <h1>Software anecdotes</h1>
+      <Menu />
+      <AnecdoteList anecdotes={anecdotes} />
+      <About />
+      <CreateNew addNew={addNew} />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
