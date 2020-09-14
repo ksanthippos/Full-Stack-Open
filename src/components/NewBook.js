a531c14e75e965c141134980fb6345bf28c31ba4
mikaelrauhala <mikael.rauhala@eduespoo.fi>
2020-09-14 17:56:21 +0300

    korjattu vielä 8.10, puuttui refetchQueries

--- src/components/NewBook.js
+++ src/components/NewBook.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useMutation } from '@apollo/client'
-import { CREATE_BOOK } from '../queries'
+import { ALL_AUTHORS, ALL_BOOKS, CREATE_BOOK } from '../queries'
 
 
 const NewBook = (props) => {
@@ -10,7 +10,9 @@ const NewBook = (props) => {
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
 
-  const [ createBook ] = useMutation(CREATE_BOOK)
+  const [ createBook ] = useMutation(CREATE_BOOK, {
+    refetchQueries: [ { query: ALL_BOOKS }, { query: ALL_AUTHORS }]
+  })
 
   if (!props.show) {
     return null
