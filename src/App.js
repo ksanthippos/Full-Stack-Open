4a7544885afd3cac6f8aac7d037831f6668f2a67
mikaelrauhala <mikael.rauhala@eduespoo.fi>
2020-09-18 12:48:15 +0300

    aloitettu 8.17

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { useState } from 'react'
-import { useQuery } from '@apollo/client'
+import { useApolloClient, useQuery } from '@apollo/client'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
-// import UpdateAuthor from './components/UpdateAuthor'
+import LoginForm from './components/LoginForm'
 import { ALL_AUTHORS, ALL_BOOKS } from './queries'
 
 
 const App = () => {
   const [page, setPage] = useState('authors')
+  const [token, setToken] = useState(null)
   
   const resultA = useQuery(ALL_AUTHORS)
   const resultB = useQuery(ALL_BOOKS)
+  const client = useApolloClient()
 
   if (resultA.loading || resultB.loading) {
     return(
@@ -21,8 +23,25 @@ const App = () => {
     )
   }
 
+  if (!token) {
+    return (
+      <div>        
+        <h2>Login</h2>
+        <LoginForm
+          setToken={setToken}
+        />
+      </div>
+    )
+  }
+
   const authors = resultA.data.allAuthors
   const books = resultB.data.allBooks
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.clearStore()
+  }
 
   return (
     <div>
@@ -45,7 +64,6 @@ const App = () => {
       <NewBook
         show={page === 'add'}
       />
-
     </div>
   )
 }
